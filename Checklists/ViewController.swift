6f81fdba4ec1ffeec17fedef9f973df1c44c7e25
latyshev <latyshev831@gmail.com>
2019-07-03 19:28:08 +0300

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Евгений Латышев on 7/3/19.
+//  Copyright © 2019 Evgeny Latyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
